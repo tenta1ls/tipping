8f7ad20fa51b3777ce7a074e9bfecf4e9f0350c6
SANTOS CHICAS <5004928547@mwce111-12.local>
2020-01-23 19:27:53 -0800

    Initial Commit

---
+++ tipping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipping
+//
+//  Created by SANTOS CHICAS on 1/23/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
